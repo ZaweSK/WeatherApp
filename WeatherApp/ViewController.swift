fb645b2e792098121ac17ac15d737c2a4bdc8e87
Peter Sevcik <psevcikbn@gmail.com>
2019-03-18 06:55:18 +0100

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Peter on 18/03/2019.
+//  Copyright © 2019 Excellence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
